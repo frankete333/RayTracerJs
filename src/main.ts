04cce2b1c54bed054175b366a466b17471159c80
Franco Olivera <francoleonardoolivera@hotmail.com>
2023-12-02 02:11:00 -0300

    Add saveImage method with canvas

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-function main (): void {
-  console.log('Hello World');
+import { saveImage } from './canvas';
+
+function main () {
+  randomImage();
+}
+
+function randomImage () {
+  const width = 100;
+  const height = 100;
+  const pixels = [];
+
+  for (let y = 0; y < height; y++) {
+    const row = [];
+
+    for (let x = 0; x < width; x++) {
+      row.push({
+        r: Math.floor(Math.random() * 255),
+        g: Math.floor(Math.random() * 255),
+        b: Math.floor(Math.random() * 255)
+      });
+    }
+
+    pixels.push(row);
+  }
+
+  saveImage(pixels);
 }
 
 main();
